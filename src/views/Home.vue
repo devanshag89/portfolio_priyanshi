9c5a96ad9afa0b5286344b1ddb94fc5260e89083
priyaanshayy <122358408+priyaanshayy@users.noreply.github.com>
2023-05-01 23:39:18 +0530

    gyuj

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark menu shadow fixed-top">
     <div class="container">
       <a class="navbar-brand" href="">
-        <img src="../images/logooo.png" alt="logo image">
+        <img src="../images/logo72.png" alt="logo image">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -74,7 +74,7 @@
   <section id="campanies" class="campanies">
     <div class="container">
       <div class="row text-center">
-        <h4 class="fw-bold lead mb-3">Trusted by campanies like</h4>
+        <h4 class="fw-bold lead mb-3">Trusted by countries like</h4>
         <div class="heading-line mb-5"></div>
       </div>
     </div>
@@ -83,39 +83,37 @@
       <div class="row">
         <div class="col-md-4 col-lg-2">
           <div class="campanies__logo-box shadow-sm">
-            <img src="../images/campanies/campany-1.png" alt="Campany 1 logo" title="Campany 1 Logo" class="img-fluid">
+            <img src="../images/campanies/pakistan.png" alt="Campany 1 logo" title="Campany 1 Logo" class="img-fluid">
           </div>
         </div>
         <div class="col-md-4 col-lg-2">
           <div class="campanies__logo-box shadow-sm">
-            <img src="../images/campanies/campany-2.png" alt="Campany 2 logo" title="Campany 2 Logo" class="img-fluid">
+            <img src="../images/campanies/usa.jpg" alt="Campany 2 logo" title="Campany 2 Logo" class="img-fluid">
           </div>
         </div>
         <div class="col-md-4 col-lg-2">
           <div class="campanies__logo-box shadow-sm">
-            <img src="../images/campanies/campany-3.png" alt="Campany 3 logo" title="Campany 3 Logo" class="img-fluid">
+            <img src="../images/campanies/uk.jpg" alt="Campany 2 logo" title="Campany 2 Logo" class="img-fluid">
           </div>
         </div>
         <div class="col-md-4 col-lg-2">
           <div class="campanies__logo-box shadow-sm">
-            <img src="../images/campanies/campany-4.png" alt="Campany 4 logo" title="Campany 4 Logo" class="img-fluid">
+            <img src="../images/campanies/india.jpg" alt="Campany 2 logo" title="Campany 2 Logo" class="img-fluid">
           </div>
         </div>
         <div class="col-md-4 col-lg-2">
           <div class="campanies__logo-box shadow-sm">
-            <img src="../images/campanies/campany-5.png" alt="Campany 5 logo" title="Campany 5 Logo" class="img-fluid">
+            <img src="../images/campanies/philippines.png" alt="Campany 2 logo" title="Campany 2 Logo" class="img-fluid">
           </div>
         </div>
         <div class="col-md-4 col-lg-2">
           <div class="campanies__logo-box shadow-sm">
-            <img src="../images/campanies/campany-6.png" alt="Campany 6 logo" title="Campany 6 Logo" class="img-fluid">
+            <img src="../images/campanies/kuwait.png" alt="Campany 2 logo" title="Campany 2 Logo" class="img-fluid">
           </div>
         </div>
-        <div class="col-md-4 col-lg-2">
-          <div class="campanies__logo-box shadow-sm">
-            <img src="../images/campanies/Delloite.png" alt="Campany 6 logo" title="Campany 6 Logo" class="img-fluid">
-          </div>
-        </div>
+        
+
+        
         
 
       </div>
